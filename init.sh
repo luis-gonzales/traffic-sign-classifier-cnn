935a691b939039cc381b9302068b965021914b9a
luis-gonzales <lr.gonzales55@gmail.com>
2018-07-24 08:30:01 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 cd data
-wget https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/traffic-signs-data.zip
-unzip traffic-signs-data.zip
-rm traffic-signs-data.zip
+gdrive download 1loP8kxxY0_0ymd07lJ5xx39RdXD58Ghs
+unzip traffic_sign_dataset.zip
+rm traffic_sign_dataset.zip
 cd ..
 cd tf_model
 gdrive download 1E2Z1zr6lTUK5wcuc4BurCQGA0Ea9cP9g
